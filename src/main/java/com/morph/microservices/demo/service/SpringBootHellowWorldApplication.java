5d2b489d032eb2130005e2482cea56ccbdda6d20
dobriencmann <dobriencann@gmail.com>
2018-10-18 14:27:14 +0100

    First commit

---
+++ src/main/java/com/morph/microservices/demo/service/SpringBootHellowWorldApplication.java
@@ -0,0 +1,15 @@
+package com.morph.microservices.demo.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHellowWorldApplication {
+	
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootHellowWorldApplication.class, args);
+
+    }
+
+}
